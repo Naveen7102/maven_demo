25501c3a65b358170f8d64532e656f40d44ddaf7
Naveen7102 <ssvnaveen@mgail.com>
2023-02-21 10:01:15 +0530

    Update Project

---
+++ src/main/java/helloworld.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class helloworld {
+    public static void main(String[] args)
+    {
+        helloworld obj = new helloworld();
+        System.out.println("Enter a number");
+        Scanner scanner  = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(factorial(n));
+    }
+    public static int factorial(int n){
+        int ans = 1;
+        for(int i=1;i<=n;i++)
+        {
+            ans = ans * i;
+        }
+        return ans;
+    }
+}
---
+++ src/test/java/helloworldTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class helloworldTest {
+    helloworld obj = new helloworld();
+    @Test
+    public void factorialTruePositive(){
+        assertEquals("Finding factorial of a number for True positive",120,obj.factorial(5));
+        assertEquals("Finding factorial of a number for True positive",24,obj.factorial(4));
+
+    }
+}
